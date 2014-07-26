f76ddbb161d8a452a30af483c207cc7ba515bf16
andhra <haiandhra007@gmail.com>
2014-07-26 07:30:11 +0000

    Navigation controller

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,19 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def faqs
+  end
+
+  def aboutus
+  end
+
+  def pricing
+  end
+
+  def contact
+  end
+
+  def features
+  end
+end
